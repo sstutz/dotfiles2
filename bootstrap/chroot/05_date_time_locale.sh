4147daae2dd38386aa157d88d3892d772b263d0d
Stefan Stutz <stefan@stutz.io>
2024-09-18 14:38:01 +0200

    add: OS bootstrapping helpers

---
+++ bootstrap/chroot/05_date_time_locale.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/^# *\(de_DE.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+cat <<EOF >/etc/locale.conf
+LANG=C.UTF-8
+LANGUAGE=C.UTF-8:en:C:de_DE.UTF-8
+LC_TIME=C.UTF-8
+LC_COLLATE=C
+LC_MEASUREMENT=metric
+LC_MESSAGES=C.UTF-8
+LC_MONETARY=de_DE.UTF-8
+LC_PAPER=de_DE.UTF-8
+EOF
+
+#timedatectl set-timezone "Europe/Berlin"
+ln -sf "/usr/share/zoneinfo/Europe/Berlin" /etc/localtime
+
+#timedatectl set-local-rtc 0
+hwclock --systohc --utc
+
+# use systemd-timesyncd for NTP synchronisation
+
+#timedatectl set-ntp true
+systemctl enable systemd-timesyncd
